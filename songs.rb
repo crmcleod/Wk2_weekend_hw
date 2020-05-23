e1ebb11c9824a9ab263c0ff05160f03bdd54c6f5
Craig McLeod <mcleod.craig@outlook.com>
2020-05-23 09:04:30 +0100

    Test all attrs have worked, refactored none descriptive attributes

--- songs.rb
+++ songs.rb
@@ -1,10 +1,10 @@
 class Song
 
-    attr_reader :artist, :song, :difficulty_level
+    attr_reader :artist, :title, :difficulty_level
 
-    def initialize(artist, song, difficulty_level)
+    def initialize(artist, title, difficulty_level)
         @artist = artist
-        @song = song
+        @title = title
         @difficulty_level = difficulty_level
     end
 
--- specs/songs_spec.rb
+++ specs/songs_spec.rb
@@ -14,4 +14,15 @@ class SongTest < MiniTest::Test
         @song = Song.new("Elton John", "I'm still standing", 5)
     end
 
+    def test_song_has_artist
+        assert_equal("Elton John", @song.artist())
+    end
+
+    def test_song_has_name
+        assert_equal("I'm still standing", @song.title)
+    end
+
+    def test_song_has_difficulty_level
+        assert_equal(5, @song.difficulty_level)
+    end
 end
